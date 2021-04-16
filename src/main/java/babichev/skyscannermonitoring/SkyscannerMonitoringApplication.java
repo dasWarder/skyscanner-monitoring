326f06574631c5ebcd1b895b5e7f61dfec8b0c8e
Andrey Babichev <Al.94_94@mail.ru>
2021-04-16 09:26:29 +0300

    Create a skeleton of the application

---
+++ src/main/java/babichev/skyscannermonitoring/SkyscannerMonitoringApplication.java
@@ -0,0 +1,13 @@
+package babichev.skyscannermonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyscannerMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyscannerMonitoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/babichev/skyscannermonitoring/SkyscannerMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package babichev.skyscannermonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyscannerMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
